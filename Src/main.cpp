eb3d590ed69543f3dec4c78ba4fe9529ceec9e04
Matthew-Corbett-96 <mcorbett16bf@gmail.com>
2020-07-28 17:35:03 -0400

    minor fixes

--- Src/main.cpp
+++ Src/main.cpp
@@ -12,29 +12,31 @@
 
 int main()
 {
-    
-    DoublyLinkedList list2("Matthew");
+    // DoublyLinkedList<char> list;
+    // list.append('p');
+    // list.append('p');
+    // list.append('p');
+    // list.append('p');
+    // list.append('p');
+    // list.append('p');
+    // DoublyLinkedList<char> list2;
+    // list2 = list;
+    // auto word = list2.toString();
+    // Log word End;
 
-    Log "List 2: " New_line
-        "Size: " And list2.getSize() End;
-    auto varr = list2.toString();
-    Log varr;
-    Skip;
-//    list1.append('!');
-    //   list2.prepend('!');
- //   list1.prepend('!');
- //   list2.set(2, 'M');
-//    list1.set(2, 'M');
-
-    // std::cout << "List 1: \n";
-    // for (int i = 0; i < list1.getSize(); i++)
-    //     std::cout << list1[i];
-    // std::cout << std::endl;
-
-    // std::cout << "List 2: \n";
-    // for (int i = 0; i < list2.getSize(); i++)
-    //     std::cout << list1[i];
-    // std::cout << std::endl;
+    DoublyLinkedList<int> list3;
+    for(int i = 0; i < 10; i++)
+    {
+        list3.append(i);
+    }
+    DoublyLinkedList<int> list4(list3);
+    // Log "Old " And list4.getSize() End;
+    // list4 = list3;
+    // Log "New " And list4.getSize() End;
+    for(int i = 0; i < list4.getSize(); i++)
+    {
+        Log list4[i] End;
+    }
 
     return 0;
 }
